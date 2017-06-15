a1585bb0754cdf46f1fc0a1f3f03d7928186548e
Qinglong Zeng <qinglong1230@gmail.com>
2017-06-15 20:09:22 +0000

    Demo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,7 +8,13 @@ def main():
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+  return render_template('index.html',data=[{'name':'open'}, {'name':'close'},{'name':'high'}, {'name':'low'},{'name':'volume'}])
+
+@app.route('/post_stock',methods=['POST'])
+def post_stock():
+  request.form['stock_ticker']
+  request.form.get('type_select')
+  return redirect(url_for('index'))
 
 if __name__ == '__main__':
   app.run(port=33507)
