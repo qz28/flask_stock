9b9ab3bf7d2972fad1ef00824cda6d5cd8d05192
Qinglong Zeng <qinglong1230@gmail.com>
2017-06-15 20:48:00 +0000

    Add plot

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from .stock_plot import stockPlot
 
 app = Flask(__name__)
 
@@ -10,11 +11,14 @@ def main():
 def index():
   return render_template('index.html',data=[{'name':'open'}, {'name':'close'},{'name':'high'}, {'name':'low'},{'name':'volume'}])
 
+
 @app.route('/post_stock',methods=['POST'])
 def post_stock():
-  request.form['stock_ticker']
-  request.form.get('type_select')
-  return redirect(url_for('index'))
+  found_or_not=stockPlot(request.form['stock_ticker'], request.form.get('type_select'))
+  if found_or_not:
+    return render_template('stocks.html')
+  else:
+    return render_template('errorpage.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
---
+++ stock_plot.py
@@ -0,0 +1,34 @@
+import requests
+import pandas as pd
+import os
+from bokeh.plotting import figure, show output_file
+
+
+def data_access(ticker, price_type):
+    r=requests.get('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker='+ticker+'&qopts.columns=date,'+price_type+'&api_key=ZqRzFiv9b9xtTx-Qs5xA')
+    rj=r.json()
+    data=rj['datatable']['data']
+    if len(data)>0:
+        df=pd.DataFrame(data,columns=['date',price_type])
+        df['date'] = pd.to_datetime(df['date'])
+        return df
+    else:
+        return False
+    
+def interactive_plot(df, ticker):
+    p1 = figure(x_axis_type="datetime", title="Stock Data for "+ticker+" from QUANDLE WIKI Set")
+    p1.grid.grid_line_alpha=0.3
+    p1.xaxis.axis_label = 'Date'
+    p1.yaxis.axis_label = df.columns[1]
+
+    p1.line(df.ix[:,0], df.ix[:,1])
+    os.chdir("./templates")
+    output_file("stocks.html", title="stocks.py example")
+
+def stockPlot(ticker_code, plot_type):    
+    stock_data=data_access(ticker_code,plot_type)
+    if not isinstance(stock_data, bool):
+        interactive_plot(stock_data, ticker_code)
+        return True
+    else:
+        return False
